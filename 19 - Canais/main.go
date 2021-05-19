f7362ae0b0f9c8fbbfc4e6ae51e919b3737942d3
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2021-05-19 01:28:08 -0300

    goroutines

---
+++ 19 - Canais/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string, done chan string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+	done <- "Terminei"
+}
+func main() {
+	done := make(chan string)
+	go say("world", done)
+	fmt.Println(<-done)
+}
